7891d69c6d4367927c5e74463f8448e04be22df4
Marcos Santos <marcospaulo1262@gmail.com>
2017-05-24 18:48:17 -0300

    Backend para consultar chamados, mais um pouco de documentação

--- src/dao/DAOChamado.java
+++ src/dao/DAOChamado.java
@@ -2,6 +2,7 @@ package dao;
 
 import dao.bd.ConnectionFactory;
 import model.Chamado;
+import model.Usuario;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -9,6 +10,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -21,53 +24,77 @@ public class DAOChamado {
     private Connection conexao;
 
     /**
-     * Consulta fatia de todos os chamados (ideal para paginação)
+     * Consulta fatia de todos os chamados (ideal para paginação),
+     * considerando os atributos não nulos de uma instância de {@link Chamado}.
      *
-     * @param inicio número de tupla inicial
-     * @param qtd    quantidades de tuplas
+     * @param chamado objeto que definirá os parâmetros da busca
+     * @param inicio  número de registro inicial
+     * @param qtd     quantidades de registros
      * @return Lista de chamados começando em [inicio] com [qtd] itens ou NULL quando não há resultados
      */
-    public List<Chamado> consulta(int inicio, int qtd) {
-        // TODO consultar no BD
-        return null;
+    public List<Chamado> consulta(Chamado chamado, int inicio, int qtd) {
+        ArrayList<Chamado> l = new ArrayList<>();
+        try {
+            this.conexao = new ConnectionFactory().getConnection();
+            {
+                String sql = "SELECT * FROM chamado WHERE ";
+
+                if (chamado.getId() != 0)
+                    sql += "idchamado='" + chamado.getId() + "' ";
+                else sql += "true ";
+                if (!chamado.getTitulo().equals(""))
+                    sql += "AND titulo='" + chamado.getTitulo() + "' ";
+                else sql += "AND true ";
+                if (chamado.getPrioridade() != null)
+                    sql += "AND prioridade='" + chamado.getPrioridade() + "' ";
+                else sql += "AND true ";
+                if (chamado.getStatus() != null)
+                    sql += "AND status='" + chamado.getStatus() + "' ";
+                else sql += "AND true ";
+                if (chamado.getData() != null)
+                    sql += "AND data='" + dataHoraMysql(chamado.getData()) + "' ";
+                else sql += "AND true ";
+                if (chamado.getSolicitante() != null)
+                    sql += "AND usuario_idsolicitante='" + chamado.getSolicitante().getId() + "' ";
+                else sql += "AND true ";
+                if (chamado.getTecnico() != null)
+                    sql += "AND usuario_idtecnico='" + chamado.getTecnico().getId() + "' ";
+                else sql += "AND true ";
+
+                sql += "LIMIT " + inicio + ", " + qtd;
+
+                PreparedStatement ps = conexao.prepareStatement(sql);
+                {
+                    ResultSet rs = ps.executeQuery();
+                    if (rs.next()) {
+                        Chamado c = new Chamado();
+                        {
+                            c.setId(rs.getLong("idchamado"));
+                            c.setTitulo(rs.getString("titulo"));
+                            c.setPrioridade(rs.getString("prioridade"));
+                            c.setStatus(rs.getString("status"));
+                            c.setData(dataHoraJava(rs.getString("tipo")));
+                            c.setSolicitante(new Usuario(Long.parseLong(rs.getString("usuario_idsolicitante"))));
+                            c.setTecnico(new Usuario(Long.parseLong(rs.getString("usuario_idtecnico"))));
+                        }
+                        l.add(c);
+                    }
+                    rs.close();
+                }
+                ps.close();
+            }
+            conexao.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return l;
     }
 
     /**
-     * Consulta um chamado pelos atributos não nulos do objeto
-     *
-     * @param chamado objeto com parâmetros de consulta
-     * @return Chamado com os dados consultados ou NULL quando não há resultados
+     * Cadastra chamado (dados passados por um solicitante).
+     * @param chamado objeto com os dados a serem salvos
+     * @return Verdadeiro caso seja cadastrado com sucesso ou Falso caso contrário
      */
-//    public Usuario consulta(Usuario usuario) {
-//        Usuario usuario1 = new Usuario("","","", null);
-//
-//        try {
-//            this.conexao = new ConnectionFactory().getConnection();
-//            {
-//                String sql = "SELECT * FROM usuario WHERE login=?";
-//                PreparedStatement ps = conexao.prepareStatement(sql);
-//                ps.setString(1, usuario.getUsuario());
-//                {
-//                    ResultSet rs = ps.executeQuery();
-//                    if (rs.next()) {
-//                        usuario1.setNome(rs.getString("nome"));
-//                        usuario1.setUsuario(rs.getString("login"));
-//                        usuario1.setSenha(rs.getString("senha"));
-//                        usuario1.setTipo(rs.getString("tipo"));
-//                    }
-//                    rs.close();
-//                }
-//                ps.close();
-//            }
-//            conexao.close();
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
-//
-//        if (usuario.validaSenha(usuario1))
-//            return usuario1;
-//        else return null;
-//    }
     public boolean cadastra(Chamado chamado) {
         boolean executou;
         try {
@@ -80,7 +107,7 @@ public class DAOChamado {
                 ps.setString(1, chamado.getTitulo());
                 ps.setString(2, chamado.getPrioridade().toString());
                 ps.setString(3, Chamado.statusOpcoes.ABERTO.toString());
-                ps.setString(4, dataMysql(LocalDateTime.now()));
+                ps.setString(4, dataHoraMysql(LocalDateTime.now()));
                 ps.setString(5, String.valueOf(chamado.getSolicitante().getId()));
                 System.out.println(chamado);
                 executou = ps.execute();
@@ -93,9 +120,26 @@ public class DAOChamado {
         return executou;
     }
 
-    private String dataMysql(LocalDateTime data) {
+    /**
+     * Atualiza chamado.
+     * @param chamado objeto que definirá os atributos e valores a serem alterados
+     * @return Verdadeiro caso seja atualizado com sucesso ou Falso caso contrário
+     */
+    public boolean atualiza(Chamado chamado) {
+        return false;
+    }
+
+    private String dataHoraMysql(LocalDateTime dataHora) {
         DateTimeFormatter formatador =
-                DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        return data.format(formatador);
+                DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss");
+        return dataHora.format(formatador);
+    }
+
+    private LocalDateTime dataHoraJava(String dataHora) {
+        DateTimeFormatter fmt = new DateTimeFormatterBuilder()
+                .appendPattern("yyyy-MM-dd")
+                .appendPattern(" hh:mm:ss")
+                .toFormatter();
+        return LocalDateTime.parse(dataHora, fmt);
     }
 }
--- src/model/Facade.java
+++ src/model/Facade.java
@@ -6,6 +6,7 @@ import dao.DAOUsuario;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.List;
 
 /**
  * Interface entre visão da aplicação e implementação das funcionalidades da aplicação
@@ -13,6 +14,13 @@ import java.security.NoSuchAlgorithmException;
  * @author marcos
  */
 public class Facade {
+    /**
+     * Autentica um usuário (login e senha) no sistema
+     * (consulta no Banco de Dados).
+     * @param login login do usuário
+     * @param senha senha do usuário
+     * @return {@link Usuario} com dados consultados ou NULL quando não há resultados
+     */
     public Usuario autentica(String login, String senha) {
         DAOUsuario du = new DAOUsuario();
         Usuario usuario = du.consulta(new Usuario(login, md5(senha)));
@@ -23,6 +31,19 @@ public class Facade {
         else return null;
     }
 
+    public boolean cadastraUsuario(Usuario usuario) {
+        return false;
+    }
+
+    /**
+     * Abre um chamado no sistema
+     * (cadastra no Banco de Dados).
+     * @param titulo        titulo do chamado
+     * @param prioridade    prioridade do chamado
+     * @param descricao     descrição do chamado
+     * @param idsolicitante id do solicitante ({@link Usuario}) do chamado
+     * @return Verdadeiro caso seja aberto com sucesso ou Falso caso contrário
+     */
     public boolean abreChamado(String titulo, String prioridade, String descricao, long idsolicitante) {
         DAOChamado dc= new DAOChamado();
         Chamado chamado = new Chamado();
@@ -30,10 +51,38 @@ public class Facade {
         chamado.setPrioridade(prioridade);
         chamado.setSolicitante(new Usuario(idsolicitante));
         return dc.cadastra(chamado);
+//        TODO cadastrar descrição
     }
 
+    /**
+     * Consulta fatia de todos os chamados (ideal para paginação),
+     * considerando os atributos não nulos de uma instância de {@link Chamado}
+     * (consulta no Banco de Dados).
+     * @param chamado   objeto que definirá os parâmetros da busca
+     * @param inicio    número de registro inicial
+     * @param qtd       quantidades de registros
+     * @return Lista de chamados começando em [inicio] com [qtd] itens ou NULL quando não há resultados
+     */
+    public List<Chamado> consultaChamados(Chamado chamado, int inicio, int qtd) {
+//        TODO testar este método
+        return new DAOChamado().consulta(chamado, inicio, qtd);
+    }
 
+    /**
+     * Atualiza informações de um chamado
+     * (atualiza no Banco de Dados).
+     * @param chamado objeto que definirá os atributos e valores a serem alterados
+     * @return Verdadeiro caso seja atualizado com sucesso ou Falso caso contrário
+     */
+    public boolean atualizaChamado(Chamado chamado) {
+        return false;
+    }
 
+    /**
+     * Criptografa texto com MD5
+     * @param s texto a ser criptografado
+     * @return texto criptografado
+     */
     private String md5(String s) {
         try {
             MessageDigest m = MessageDigest.getInstance("MD5");
